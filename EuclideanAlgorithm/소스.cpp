96f3fc009e817b00d8fb0cd81a55c4601a1b1bf8
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-04-12 17:51:14 +0900

    초기 커밋.

---
+++ EuclideanAlgorithm/소스.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int get_gcd(int u, int v)
+{
+	if (u < v)
+		swap(u, v);
+	if (v <= 0)
+		return u;
+	return get_gcd(u - v, v);
+}
+
+int main()
+{
+	int n1, n2;
+	cin >> n1 >> n2;
+	cout << get_gcd(n1, n2);
+	return 0;
+}
